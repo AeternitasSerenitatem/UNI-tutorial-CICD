2872ee938603950a410d3dde15c8bc766c08deae
AeternitasSerenitatem <136620264+AeternitasSerenitatem@users.noreply.github.com>
2023-10-13 14:26:41 +0300

    Добавьте файлы проекта.

---
+++ UNI tutorial CICD/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UNI_tutorial_CICD.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
